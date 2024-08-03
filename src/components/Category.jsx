68c6f328a383dbfd2171e09ba09a9a19d836f1d4
Applestar <applestar@Applestars-MacBook-Pro.local>
2024-08-03 18:43:05 +0330

    import image in the Category.jsx

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,8 +1,21 @@
 // import React from "react";
 import styles from "./category.module.css";
+import meat from '../assets/images/meat.jpg'
+import chicken from '../assets/images/chicken.jpg'
+import rice from '../assets/images/rice.jpg'
+import pasta from '../assets/images/pasta.jpg'
+import pizza from '../assets/images/pizza.jpg'
+import salad from '../assets/images/salad.jpg'
 
 function Category({ setQuery }) {
-  const foodArray = ["chicken", "meat", "pasta", "pizza", "salad", "rice"];
+  const foodArray = [
+    { name: "chicken", img: chicken },
+    { name: "meat", img: meat },
+    { name: "salad", img: salad },
+    { name: "rice", img: rice },
+    { name: "pizza", img: pizza },
+    { name: "pasta", img: pasta }
+  ]
   return (
     <div>
       <div className={styles.details}>
@@ -10,7 +23,7 @@ function Category({ setQuery }) {
           return (
             <div className={styles.detail}>
               <button className={styles.btnCategory} onClick={() => setQuery(food)}>
-                <img src={`../../public/image/${food}.jpg`} className={styles.img} title={food}/>
+                <img src={food.img} className={styles.img} title={food.name} />
               </button>
             </div>
           );
